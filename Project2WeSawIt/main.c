cf658fc354d5321545a8f07e8cc07d98623fb97d
sahizidanlukman <sahizidanlukman@gmail.com>
2026-03-06 21:14:48 +0700

    Test 2

--- Project2WeSawIt/main.c
+++ Project2WeSawIt/main.c
@@ -4,6 +4,7 @@ int main() {
     printf("Hello, World!\n");
     printf("Welcome to C programming.\n");
     printf("This is a simple C program.\n");
+    printf("C is a powerful programming language.\n");
     return 0;
 }
 
