7e146eaf19246ab0bf261f5e3a0e216811789428
sahizidanlukman <sahizidanlukman@gmail.com>
2026-03-29 20:21:17 +0700

    Menambahkan fitur find and replace

---
+++ Project2WeSawIt/zidan.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "zidan.h"
+
+void findAndReplace() {
+    char filename[100];
+    char find[100], replace[100];
+    char buffer[1000];
+    char result[5000] = "";
+
+    FILE *fp;
+
+    printf("Masukkan nama file: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("File tidak ditemukan!\n");
+        return;
+    }
+
+    printf("Kata yang ingin dicari: ");
+    scanf("%s", find);
+
+    printf("Kata pengganti: ");
+    scanf("%s", replace);
+
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        char temp[1000];
+        char *pos, *start = buffer;
+
+        while ((pos = strstr(start, find)) != NULL) {
+            strncpy(temp, start, pos - start);
+            temp[pos - start] = '\0';
+
+            strcat(result, temp);
+            strcat(result, replace);
+
+            start = pos + strlen(find);
+        }
+
+        strcat(result, start);
+    }
+
+    fclose(fp);
+
+    fp = fopen(filename, "w");
+    if (fp == NULL) {
+        printf("Gagal membuka file!\n");
+        return;
+    }
+
+    fputs(result, fp);
+    fclose(fp);
+
+    printf("Berhasil replace kata!\n");
+}
