a7b4cb44208ffae4e2f23420536ec6fab3abfed0
irfanardiansah320-oss <irfanardiansah320@gmail.com>
2026-04-02 09:22:41 +0700

    fix: Perbaikan pada tampilan menu yang awalnya berada pada irfanh1.c dipindahkan ke main.c

--- Project2WeSawIt/main.c
+++ Project2WeSawIt/main.c
@@ -1,6 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "irfan1.h"
+#include "zidan.h"
+#include "rere1.h"
 
 int main() {
-    mainMenu();
+    int choice;
+
+    do {
+        #ifdef _WIN32
+        system("cls");
+        #else
+        system("clear");
+        #endif
+
+        printf("\033[1;34m");
+        printf("===========================================\n");
+        printf("|            \033[1;33mTEXT EDITOR MENU\033[1;34m             |\n");
+        printf("===========================================\n");
+        printf("| \033[1;32m1. Create New File\033[1;34m                      |\n");
+        printf("| \033[1;32m2. Open File\033[1;34m                            |\n");
+        printf("| \033[1;32m3. Edit File\033[1;34m                            |\n");
+        printf("| \033[1;32m4. Find and Replace\033[1;34m                     |\n");
+        printf("| \033[1;31m5. Exit Editor\033[1;34m                          |\n");
+        printf("===========================================\n");
+        printf("\033[1;37mEnter your choice: \033[0m");
+
+        scanf("%d", &choice);
+        while (getchar() != '\n'); // bersihin buffer
+
+        switch(choice) {
+            case 1:
+                createNewFile();
+                break;
+
+            case 2:
+                openFile();
+                break;
+
+            case 3:
+                editFile();
+                break;
+
+            case 4: {
+                findAndReplace(filename);
+                break;
+            }
+
+            case 5:
+                exitEditor();
+                break;
+
+            default:
+                printf("\033[1;31mInvalid choice! Please try again.\033[0m\n");
+        }
+
+        if (choice != 5) {
+             printf("\033[1;33mPress Enter to continue...\033[0m");
+            getchar();
+            }
+
+    } while(choice != 5);
+
     return 0;
 }
