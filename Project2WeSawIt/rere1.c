79089ae272dbdc717e43403a6e59b93d33c191c5
sahizidanlukman <sahizidanlukman@gmail.com>
2026-04-02 09:43:31 +0700

    Revisi rere1.c

--- Project2WeSawIt/rere1.c
+++ Project2WeSawIt/rere1.c
@@ -3,92 +3,6 @@
 #include <string.h>
 
 // Prototipe fungsi
-void createNewFile();
-void openFile();
-void editFile();
-void exitEditor();
-void mainMenu();
-
-void mainMenu() {
-    int choice;
-    do {
-        #ifdef _WIN32
-        system("cls"); // Untuk Windows
-        #else
-        system("clear"); // Untuk Linux/Mac
-        #endif
-        
-        printf("\033[1;34m"); // Warna biru
-        printf("===========================================\n");
-        printf("|            \033[1;33mTEXT EDITOR MENU\033[1;34m             |\n");
-        printf("===========================================\n");
-        printf("| \033[1;32m1. Create New File\033[1;34m                      |\n");
-        printf("| \033[1;32m2. Open File\033[1;34m                            |\n");
-        printf("| \033[1;32m3. Edit File\033[1;34m                            |\n");
-        printf("| \033[1;31m4. Exit Editor\033[1;34m                          |\n");
-        printf("===========================================\n");
-        printf("\033[1;37mEnter your choice: \033[0m");
-        scanf("%d", &choice);
-
-        // Bersihkan buffer input setelah scanf
-        while (getchar() != '\n');
-
-        switch(choice) {
-            case 1:
-                createNewFile();
-                break;
-            case 2:
-                openFile();
-                break;
-            case 3:
-                editFile();
-                break;
-            case 4:
-                exitEditor();
-                break;
-            default:
-                printf("\033[1;31mInvalid choice! Please try again.\033[0m\n");
-        }
-
-        if (choice != 4) {
-            printf("\033[1;33mPress Enter to continue...\033[0m");
-            getchar(); // Tunggu Enter
-        }
-    } while(choice != 4);
-}
-
-void createNewFile() {
-    char filename[100];
-    FILE *file;
-    char buffer[256];
-
-    printf("\n\033[1;36m=== CREATE NEW FILE ===\033[0m\n");
-    printf("Enter filename to create: ");
-    fgets(filename, sizeof(filename), stdin);
-    filename[strcspn(filename, "\n")] = '\0'; // Hapus newline di akhir
-
-    file = fopen(filename, "w");
-    if (file == NULL) {
-        printf("\033[1;31mError creating file!\033[0m\n");
-        return;
-    }
-
-    printf("Enter your text (type a blank line to finish):\n");
-    printf("\033[1;32m");
-    while (1) {
-        fgets(buffer, sizeof(buffer), stdin);
-        // Jika baris kosong (hanya newline), berhenti
-        if (strcmp(buffer, "\n") == 0) {
-            break;
-        }
-        fputs(buffer, file);
-    }
-    printf("\033[0m");
-
-    fclose(file);
-    printf("\033[1;32mFile '%s' created successfully.\033[0m\n", filename);
-}
-
 void openFile() {
     char filename[100];
     FILE *file;
@@ -144,9 +58,4 @@ void editFile() {
 
     fclose(file);
     printf("\033[1;32mFile '%s' saved successfully!\033[0m\n", filename);
-}
-
-void exitEditor() {
-    printf("\n\033[1;35mExiting the editor. Goodbye!\033[0m\n");
-    exit(0);
 }
