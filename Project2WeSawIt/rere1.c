0a832039330dd03bf83925aa9250cfe9fd6a8785
Cruass <rayhanfarid20@gmail.com>
2026-04-09 19:41:57 +0700

    fixed: main & fitur rayhan

--- Project2WeSawIt/rere1.c
+++ Project2WeSawIt/rere1.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 void openFile() {
     char filename[100];
+    FILE *file;
+
     printf("\n\033[1;36m=== OPEN FILE ===\033[0m\n");
     printf("Enter the filename to open: ");
     fgets(filename, sizeof(filename), stdin);
     filename[strcspn(filename, "\n")] = '\0';
 
-    FILE *file = fopen(filename, "r");
+    file = fopen(filename, "r");
     if (file == NULL) {
         printf("\033[1;31mFile '%s' not found!\033[0m\n", filename);
         return;
@@ -19,19 +20,54 @@ void openFile() {
     printf("\033[1;33mFile contents:\033[0m\n");
     printf("----------------------------------------\n");
     char ch;
-    while ((ch = fgetc(file)) != EOF) putchar(ch);
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
     printf("\n----------------------------------------\n");
+
     fclose(file);
 
     char choice;
-    printf("\nApakah kamu ingin mengedit file ini dengan editor kursor? (y/n): ");
+    printf("\nApakah kamu ingin mengedit file ini? (y/n): ");
     scanf(" %c", &choice);
     getchar(); 
 
     if (choice == 'y' || choice == 'Y') {
-        runEditor(filename, 0);   // 0 = file sudah ada, muat isinya
-        printf("\033[1;32mFile '%s' telah disimpan.\033[0m\n", filename);
-    } else {
-        printf("File tidak diedit.\n");
+
+        file = fopen(filename, "r");
+        printf("\n\033[1;33mCurrent content:\033[0m\n");
+        printf("----------------------------------------\n");
+
+        while ((ch = fgetc(file)) != EOF) {
+            putchar(ch);
+        }
+
+        fclose(file);
+        printf("\n----------------------------------------\n");
+
+        file = fopen(filename, "a");
+        if (file == NULL) {
+            printf("\033[1;31mError opening file!\033[0m\n");
+            return;
+        }
+
+        char text[1000];
+        printf("Add new content (end with blank line):\n");
+        printf("\033[1;32m");
+
+        fputs("\n", file);
+
+        while (1) {
+            fgets(text, sizeof(text), stdin);
+            if (strcmp(text, "\n") == 0) {
+                break;
+            }
+            fputs(text, file);
+        }
+
+        printf("\033[0m");
+        fclose(file);
+
+        printf("\033[1;32mFile '%s' updated successfully!\033[0m\n", filename);
     }
 }
