0ab64fc9cd9f771ebf7fb5851b9ac6e002bb5100
sahizidanlukman <sahizidanlukman@gmail.com>
2026-05-05 14:30:07 +0700

    revisi MAX_COLS

--- Project2WeSawIt/zidan.c
+++ Project2WeSawIt/zidan.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "edit_cursor.h"
 #include "zidan.h"
 
 
@@ -82,7 +83,7 @@ void findAndReplace() {
     printf("Berhasil replace kata!\n");
 }
 
-void handleCursorMovement(int ch, int *cursorRow, int *cursorCol, int rowCount, char text[][256])
+void handleCursorMovement(int ch, int *cursorRow, int *cursorCol, int rowCount, char text[][MAX_COLS])
 {
     if (ch == 72 && *cursorRow > 0) { // naik
         (*cursorRow)--; 
