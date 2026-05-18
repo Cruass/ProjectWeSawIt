7693cd57707a4c119c784458322bae37ba38df99
Cruass <rayhanfarid20@gmail.com>
2026-05-18 23:25:37 +0700

    update fitur rere

--- rere1.h
+++ rere1.h
@@ -1,8 +1,20 @@
 #ifndef RERE1_H
 #define RERE1_H
 
+#include "linkedlist.h"
+
+// Helper
+Node* getNodeAt(Cursor *cursor, int index);
+
+// Tampilan
+void tampilkanNode(Cursor *cursor);
+void simpanKeFile(Cursor *cursor, const char *filename);
+
+// Menu utama
 void openFile();
-void editFile();
-void deletefile();
+// void deletefile();
+
+// Editor linked list
+void editLinkedList(Cursor *cursor, const char *filename);
 
 #endif
